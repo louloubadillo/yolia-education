572e978461219370da33194ed1f4545d892b34bf
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2021-10-07 12:03:21 -0500

    add basic question structure

--- tercero.js
+++ tercero.js
@@ -10,15 +10,19 @@ let colors = [
 
 let z = new Preguntas();
 
-addQuestion('0', 'auto1')
-addQuestion('1', 'seccion2')
-addQuestionName()
+addQuestions('0', 'auto1')
 
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
 function checkAnswers(id) {
+    let currSection = document.querySelector(id);
+
     // Select all questions
-    let questions = document.querySelectorAll(id + ' .pregunta');
-    let amt = 0;
+    let questions = currSection.querySelectorAll('.pregunta');
+    let totalAmt = 0;
+    let correctAmt = 0;
 
     for (let i = 0; i < questions.length; i++) {
         // Get inputs from each question
@@ -33,13 +37,13 @@ function checkAnswers(id) {
                 ele.style.background = '#fff0'
                 ele.style.color = 'black';
                 ele.style.fontWeight = 'bolder';
-                ele.parentNode.classList.add("correct");
                 ele.parentNode.classList.remove("incorrect");
+                correctAmt++;
             } else {
                 ele.parentNode.classList.add("incorrect");
             }
             ele.style.borderColor = "#fff0";
-            amt++;
+            totalAmt++;
         }
 
         // Select mover elements (if there are any)
@@ -51,13 +55,14 @@ function checkAnswers(id) {
         
             for (const key in b) {
                 let ele = document.getElementById(key);
-                amt++;
+                totalAmt++;
                 if (z.isCorrect(key, b[key])) {
                     ele.style.backgroundColor = '#b4f7b7';
                     ele.style.cursor = "default";
                     ele.setAttribute("draggable", "false");
                     ele.classList.add("correct");
                     ele.classList.remove("incorrect");
+                    correctAmt++;
                 } else {
                     ele.classList.add("incorrect");
                     ele.classList.remove("dropped")
@@ -72,7 +77,7 @@ function checkAnswers(id) {
             icon.setAttribute("aria-hidden","true");
         }
 
-        if (questions[i].querySelectorAll(' .incorrect').length == 0) {
+        if (questions[i].querySelectorAll('.incorrect').length == 0) {
             icon.className = 'fa fa-check';
             icon.style.backgroundColor = '#25AC8A';
             // Color question block accordingly
@@ -90,8 +95,44 @@ function checkAnswers(id) {
     }
 
     // Show score for section
-    let scoreDiv = document.querySelector(id + ' .report');
-    scoreDiv.innerHTML = `${document.querySelectorAll(id + ' .correct').length}/${amt}`;
+    let scoreDiv = document.createElement('div');
+    scoreDiv.classList.add('report');
+    scoreDiv.innerHTML = `${correctAmt}/${totalAmt}`;
+    currSection.appendChild(scoreDiv);
+    if(correctAmt == totalAmt) {
+        alert('Felicidades!')
+    }
+}
+
+function addOperation(key, ints, sign, prgn) {
+    prgn.className = 'pregunta';
+    prgn.style = 'padding-top: 0.5em;';
+    prgn.style.flexDirection = 'column';
+    prgn.style.backgroundColor = key[key.length-1]%2 == 0 ? '#f8f8f8' : '#fff';
+
+    let part = document.createElement('div');
+    part.className = 'parte';
+
+    let lbl = document.createElement('label');
+    lbl.className = 'sumas';
+    lbl.setAttribute('for', key);
+    let oprt = '';
+    for (let i = 0; i < ints.length-1; i++) {
+        oprt += ints[i] + '<br>'
+    }
+    oprt += sign + ' ' + ints[ints.length-1];
+    lbl.innerHTML = oprt;
+
+    let inpt = document.createElement('input');
+    inpt.type = 'number';
+    inpt.id = key;
+
+    let cnt = document.createElement('div');
+    cnt.appendChild(part);
+
+    part.appendChild(lbl);
+    part.appendChild(inpt);
+    return cnt;
 }
 
 function addQuestionName() {
@@ -109,102 +150,92 @@ function addQuestionName() {
     }
 }
 
-function addQuestion(index, id) {
-    let preguntas = z.getSection(index);
+function addBasic(data, key, currDiv) {
+    let ints = [];
+        
+    let img = document.createElement('div');
+    img.style.flexDirection = "row";
+    img.style.paddingBottom = 0;
+    img.style.marginBottom = 0;
+    
+    let text = data[1];
 
-    for (const key in preguntas) {
-        let text = preguntas[key][0];
-        let int = [];
+    // Generate numbers and images
+    for (let i = 0; i < data[2].length; i++) {
+        // Generate random numbers and add them to the text
+        let randInt = getRandom(data[2][i][1], data[2][i][2]);
+        ints.push(randInt);
+        text = text.replace(data[2][i][0], randInt);
 
-        let img = document.createElement('div');
-        img.style.flexDirection = "row";
-        img.style.paddingBottom = 0;
-        img.style.marginBottom = 0;
+        // Generate amount of images necessary
+        let dv = document.createElement('div');
+        let img_i = document.createElement('i');
+        img_i.className = data[4][i];
+        let sty = `color: ${colors[getRandom(0,colors.length)]};`;
+        let caption = document.createElement('p');
+        caption.innerHTML = randInt;
 
-        // Generate numbers and images
-        for (let i = 0; i < preguntas[key][1].length; i++) {
-            // Generate random numbers and add them to the text
-            let randInt = getRandom(preguntas[key][1][i][1], preguntas[key][1][i][2]);
-            int.push(randInt);
-            text = text.replace(preguntas[key][1][i][0], randInt);
-
-            // Generate amount of images necessary
-            let dv = document.createElement('div');
-            let img_i = document.createElement('i');
-            img_i.className = preguntas[key][3][i];
-            let sty = `color: ${colors[getRandom(0,colors.length)]};`;
-            let caption = document.createElement('p');
-            caption.innerHTML = randInt;
-
-            // If any image has a particular styling
-            if(preguntas[key][4] != undefined) {
-                for (let j = 0; j < preguntas[key][4].length; j++) {
-                    sty += preguntas[key][4][i];
-                }
+        // If any image has a particular styling
+        if(data[5] != undefined) {
+            for (let j = 0; j < data[5].length; j++) {
+                sty += data[5][i];
             }
-
-            img_i.style = sty;
-
-            dv.appendChild(img_i);
-            dv.appendChild(caption);
-            img.appendChild(dv);
         }
 
-        let currDiv = document.getElementById(id);
+        img_i.style = sty;
 
-        let txtDiv = document.createElement('p');
-        txtDiv.className = 'pregunta-texto';
-        txtDiv.innerHTML = text;
-        txtDiv.style.backgroundColor = key[key.length-1]%2 == 0 ? '#f8f8f8' : '#fff';
-
-        currDiv.appendChild(txtDiv);
-
-        let prgn = document.createElement('div');
-        prgn.className = 'pregunta';
-        prgn.style = 'padding-top: 0.5em;';
-        prgn.style.flexDirection = 'column';
-        prgn.style.backgroundColor = key[key.length-1]%2 == 0 ? '#f8f8f8' : '#fff';
-
-        let part = document.createElement('div');
-        part.className = 'parte';
-
-        let lbl = document.createElement('label');
-        lbl.className = 'sumas';
-        lbl.setAttribute('for', key);
-        let oprt = '';
-        for (let i = 0; i < int.length-1; i++) {
-            oprt += int[i] + '<br>'
-        }
-        oprt += preguntas[key][2] + ' ' + int[int.length-1];
-        lbl.innerHTML = oprt;
-
-        let inpt = document.createElement('input');
-        inpt.type = 'number';
-        inpt.id = key;
-
-        let cnt = document.createElement('div');
-        cnt.appendChild(part);
-
-        part.appendChild(lbl);
-        part.appendChild(inpt);
-        prgn.appendChild(img);
-        prgn.appendChild(cnt);
-        currDiv.appendChild(prgn);
-
-        // Save answer
-        let k = 0;
-        let symbol = preguntas[key][2];
-        if(symbol == "+") {
-            for(const i in int) { k += int[i]; }
-        } else if (symbol == "-") {
-            for(const i in int) { k -= int[i]; }
-        } else {
-            
-        }
-        z.setAnswer(key, k);
+        dv.appendChild(img_i);
+        dv.appendChild(caption);
+        img.appendChild(dv);
     }
+
+    let txtDiv = document.createElement('p');
+    txtDiv.className = 'pregunta-texto';
+    txtDiv.innerHTML = text;
+    txtDiv.style.backgroundColor = key[key.length-1]%2 == 0 ? '#f8f8f8' : '#fff';
+
+    currDiv.appendChild(txtDiv);
+    let prgn = document.createElement('div');
+    let cnt = addOperation(key, ints, data[3], prgn);
+    
+    prgn.appendChild(img);
+    prgn.appendChild(cnt);
+    currDiv.appendChild(prgn);
+
+    // Save answer
+    let k = 0;
+    switch (data[3]) {
+        case "+":
+            for(const i in ints) { k += ints[i]; }
+            break;
+        case "-":
+            k = ints[0];
+            for (let i = 1; i < ints.length; i++) { k -= ints[i]; }
+            break;
+        default:
+            break;
+    }
+    z.setAnswer(key, k);
 }
 
-function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+function addSort() {
+    
+}
+
+function addQuestions(index, id) {
+    let preguntas = z.getSection(index);
+    let currDiv = document.getElementById(id);
+
+    for (const key in preguntas) {
+        switch (preguntas[key][0]) {
+            case "basic":
+                addBasic(preguntas[key], key, currDiv);
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    addQuestionName()
 }
