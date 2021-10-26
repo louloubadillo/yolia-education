ef0be4b3727bc8f7a687f8631124e55f75ae39cc
Miguel Arriaga <marriagav3@gmail.com>
2021-10-26 17:46:36 -0500

    more exercises

---
+++ cuarto-js/cuarto-10.js
@@ -0,0 +1,54 @@
+let counter101=3
+
+let tag10_1 = document.createElement("p");
+let text10_1 = document.createTextNode("");
+tag10_1.appendChild(text1);
+let element10_1 = document.getElementById("cuarto10_1");
+element10_1.appendChild(tag10_1);
+
+let tag10_2 = document.createElement("p");
+let text10_2 = document.createTextNode("");
+tag10_2.appendChild(text1);
+let element10_2 = document.getElementById("cuarto10_2");
+element10_2.appendChild(tag10_2);
+
+let tag10_3 = document.createElement("p");
+let text10_3 = document.createTextNode("");
+tag10_3.appendChild(text1);
+let element10_3 = document.getElementById("cuarto10_3");
+element10_3.appendChild(tag10_3);
+
+let tag10_4 = document.createElement("p");
+let text10_4 = document.createTextNode("");
+tag10_4.appendChild(text1);
+let element10_4 = document.getElementById("cuarto10_4");
+element10_4.appendChild(tag10_4);
+
+function checkAnswers10(id){
+    if (counter101==0){
+        return;
+    }
+    newID=id[5]
+    let tag;
+    if (id[3]=="1"){
+        tag=tag10_1;
+    }
+    else if (id[3]=="2"){
+        tag=tag10_2;
+    }
+    else if (id[3]=="3"){
+        tag=tag10_3;
+    }
+    else if (id[3]=="4"){
+        tag=tag10_4;
+    }
+    if (newID=="1"){
+        tag.textContent= "Correcto!";
+    }
+    else if (newID=="2"){
+        counter101--;
+        tag.textContent= "Incorrecto!";
+    }
+    let tries=document.getElementById("tries_cuarto10");
+    tries.textContent="Intentos restantes: " +counter101;
+}
---
+++ cuarto-js/cuarto-9.js
@@ -0,0 +1,54 @@
+let counter91=3
+
+let tag9_1 = document.createElement("p");
+let text9_1 = document.createTextNode("");
+tag9_1.appendChild(text1);
+let element9_1 = document.getElementById("cuarto9_1");
+element9_1.appendChild(tag9_1);
+
+let tag9_2 = document.createElement("p");
+let text9_2 = document.createTextNode("");
+tag9_2.appendChild(text1);
+let element9_2 = document.getElementById("cuarto9_2");
+element9_2.appendChild(tag9_2);
+
+let tag9_3 = document.createElement("p");
+let text9_3 = document.createTextNode("");
+tag9_3.appendChild(text1);
+let element9_3 = document.getElementById("cuarto9_3");
+element9_3.appendChild(tag9_3);
+
+let tag9_4 = document.createElement("p");
+let text9_4 = document.createTextNode("");
+tag9_4.appendChild(text1);
+let element9_4 = document.getElementById("cuarto9_4");
+element9_4.appendChild(tag9_4);
+
+function checkAnswers9(id){
+    if (counter91==0){
+        return;
+    }
+    newID=id[4]
+    let tag;
+    if (id[2]=="1"){
+        tag=tag9_1;
+    }
+    else if (id[2]=="2"){
+        tag=tag9_2;
+    }
+    else if (id[2]=="3"){
+        tag=tag9_3;
+    }
+    else if (id[2]=="4"){
+        tag=tag9_4;
+    }
+    if (newID=="1"){
+        tag.textContent= "Correcto!";
+    }
+    else if (newID=="2"){
+        counter91--;
+        tag.textContent= "Incorrecto!";
+    }
+    let tries=document.getElementById("tries_cuarto9");
+    tries.textContent="Intentos restantes: " +counter91;
+}
