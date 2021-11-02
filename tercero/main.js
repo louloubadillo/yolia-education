383eaa07d6d8bdd5403cdbebed04ae24478e2c37
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2021-11-01 19:43:33 -0600

    add overall score

--- tercero/main.js
+++ tercero/main.js
@@ -13,11 +13,11 @@ sections.forEach((section, i) => {
     });
 
     // Add score button at the end of each section
-    const button = `<div class="center"><button onclick="showScore(${i})" class="verify check-score">Calcular puntuación</button></div>`;
+    const button = `<div class="center"><button onclick="calcSectionScore(${i})" class="verify check-score">Siguiente sección</button></div>`;
     section.insertAdjacentHTML('beforeend', button);
 });
 
-function showScore(i) {
+function calcSectionScore(i) {
     // Calculate score
     let total = 0;
     let correct = 0;
@@ -38,6 +38,43 @@ function showScore(i) {
         scoreText.style.color = '#25AC8A';
     } else if (score < 60) {
         scoreText.style.color = '#FF0076'
+    } else {
+        scoreText.style.color = '#ff9a00';
     }
     scoreText.innerHTML = `${score}%`;
+
+    // Close current section
+    sections[i].removeAttribute('open');
+
+    if(i < 14) {
+        // Open next section
+        sections[i+1].setAttribute('open','');
+
+        // Focus on next section
+        document.getElementById(`section-${i+1}`).scrollIntoView();
+    }
+
+}
+
+function showTotalScore() {
+    let totalScore = 0;
+
+    // Add up all section scores
+    sections.forEach(section => {
+        const scoreText = section.nextElementSibling;
+        totalScore += parseFloat(scoreText.innerHTML) ? parseFloat(scoreText.innerHTML) : 0;
+    });
+
+    totalScore /= 15;
+    const totalText = document.getElementById('total-score');
+
+    if(totalScore >= 90) {
+        totalText.style.color = '#25AC8A';
+    } else if (totalScore < 60) {
+        totalText.style.color = '#FF0076'
+    } else {
+        totalText.style.color = '#ff9a00';
+    }
+
+    totalText.innerHTML = `<em>Calificación: </em>${totalScore.toFixed(2)}`;
 }
